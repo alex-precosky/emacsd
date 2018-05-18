f1adc20015e8bf81e77e38687dc698444ab2efad
Alex Precosky <Alex.Precosky@fpinnovations.ca>
2018-05-18 09:59:48 -0700

    Set up org-capture templates

--- settings/setup-org.el
+++ settings/setup-org.el
@@ -7,5 +7,12 @@
 
 (setq org-agenda-files (list (concat org-directory "/notes.org")))
 
+
+(setq org-capture-templates
+ '(("t" "Todo" entry (file+headline (concat org-directory "/notes.org") "Incoming Tasks")
+        "* TODO %?\n  %i\n")
+   ("j" "Journal" entry (file+olp+datetree (concat org-directory "/journal.org"))
+        "* %?\nEntered on %U\n  %i\n  %a")))
+
 (provide 'setup-org)
 
