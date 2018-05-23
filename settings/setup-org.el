947fe7b01e4fdc253f3f1bd85e52fb0249380ca2
Alex Precosky <precosky@mac.com>
2018-05-22 21:10:03 -0700

    Only sets up org-directory stuff if org-directory is defined

--- settings/setup-org.el
+++ settings/setup-org.el
@@ -5,14 +5,14 @@
 
 (setq org-log-done 'time)
 
-(setq org-agenda-files (list (concat org-directory "/notes.org")))
+(if (boundp 'org-directory)
+    (setq org-agenda-files (list (concat org-directory "/notes.org")))
 
-
-(setq org-capture-templates
- '(("t" "Todo" entry (file+headline (concat org-directory "/notes.org") "Incoming Tasks")
+  (setq org-capture-templates
+	'(("t" "Todo" entry (file+headline (concat org-directory "/notes.org") "Incoming Tasks")
         "* TODO %?\n  %i\n")
    ("j" "Journal" entry (file+olp+datetree (concat org-directory "/journal.org"))
         "* %?\nEntered on %U\n  %i\n  %a")))
-
+) ;endif
 (provide 'setup-org)
 
