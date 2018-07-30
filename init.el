de8e41ad60ab79e6e8632dab31d9c6bd5ad1f5b3
precosky <precosky@amazon.com>
2018-07-30 09:14:00 -0700

    Added nyan-mode to package list to download

--- init.el
+++ init.el
@@ -92,6 +92,7 @@
      tramp
      srefactor
      cmake-mode
+     nyan-mode
      )))
 
 (condition-case nil
@@ -172,7 +173,7 @@
  '(nyan-mode t)
  '(package-selected-packages
    (quote
-    (exec-path-from-shell srefactor cmake-mode solarized-theme realgud elpy helm ggtags company-c-headers conda flymake-solidity solidity-mode powerline csv-mode nyan-mode monokai-theme jedi skewer-mode yasnippet whitespace-cleanup-mode visual-regexp undo-tree string-edit smooth-scrolling smex smartparens simple-httpd restclient prodigy paredit move-text markdown-mode magit ido-vertical-mode ido-completing-read ido-at-point htmlize highlight-escape-sequences guide-key flycheck-pos-tip flx-ido fill-column-indicator elisp-slime-nav dockerfile-mode dired-details css-eldoc))))
+    (basic-mode exec-path-from-shell srefactor cmake-mode solarized-theme realgud elpy helm ggtags company-c-headers conda flymake-solidity solidity-mode powerline csv-mode nyan-mode monokai-theme jedi skewer-mode yasnippet whitespace-cleanup-mode visual-regexp undo-tree string-edit smooth-scrolling smex smartparens simple-httpd restclient prodigy paredit move-text markdown-mode magit ido-vertical-mode ido-completing-read ido-at-point htmlize highlight-escape-sequences guide-key flycheck-pos-tip flx-ido fill-column-indicator elisp-slime-nav dockerfile-mode dired-details css-eldoc))))
 
 ;; eshell support
 (conda-env-initialize-eshell)
