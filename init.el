4c6689c585b42b486ef3ce621e41cc561d07b6cb
Alex Precosky <precosky@mac.com>
2021-02-13 14:44:40 -0800

    Swap flymake-google-cpplint for flycheck-google-cpplint

--- init.el
+++ init.el
@@ -96,7 +96,7 @@
      counsel
      swiper
      org-bullets
-     flymake-google-cpplint
+     flycheck-google-cpplint
      multiple-cursors
      flycheck-clang-analyzer
      clang-format
@@ -192,13 +192,13 @@
 ;; Font lock dash.el
 (eval-after-load "dash" '(dash-enable-font-lock))
 
-(eval-after-load 'flycheck '(require 'setup-flycheck))
-(with-eval-after-load 'flycheck
-  (require 'flycheck-clang-analyzer)
-  (flycheck-clang-analyzer-setup))
-
-(setq clang-format-style-option "google")
-
+(eval-after-load 'flycheck
+  '(progn
+     (require 'flycheck-google-cpplint)
+     ;; Add Google C++ Style checker.
+     ;; In default, syntax checked by Clang and Cppcheck.
+     (flycheck-add-next-checker 'c/c++-clang
+                                '(warning . c/c++-googlelint))))
 
 ;; python
 (elpy-enable)
@@ -267,25 +267,25 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(company-backends
-   '(company-bbdb company-nxml company-css company-eclim company-capf company-semantic company-gtags company-xcode company-cmake company-files
+   (quote
+    (company-bbdb company-nxml company-css company-eclim company-capf company-semantic company-gtags company-xcode company-cmake company-files
 		  (company-dabbrev-code company-clang company-etags company-keywords)
-		  company-oddmuse company-dabbrev))
+		  company-oddmuse company-dabbrev)))
  '(conda-anaconda-home "c:/Users/Alex/Anaconda3")
  '(custom-safe-themes
-   '("8b58ef2d23b6d164988a607ee153fd2fa35ee33efc394281b1028c2797ddeebb" "bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" "c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" "8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" "f78de13274781fbb6b01afd43327a4535438ebaeec91d93ebdbba1e3fba34d3c" default))
+   (quote
+    ("d9646b131c4aa37f01f909fbdd5a9099389518eb68f25277ed19ba99adeb7279" "8b58ef2d23b6d164988a607ee153fd2fa35ee33efc394281b1028c2797ddeebb" "bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" "c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" "8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" "f78de13274781fbb6b01afd43327a4535438ebaeec91d93ebdbba1e3fba34d3c" default)))
  '(electric-indent-mode nil)
- '(flycheck-clang-args '("-Wsign-conversion"))
- '(flycheck-clang-warnings '("all" "extra"))
+ '(flycheck-clang-args (quote ("-Wsign-conversion")))
+ '(flycheck-clang-warnings (quote ("all" "extra")))
  '(nyan-mode t)
  '(package-selected-packages
-   '(rust-mode projectile git-timemachine clang-format flycheck-clang-analyzer multiple-cursors org-bullets counsel ivy-yasnippet swiper elpy helm ggtags company-c-headers conda flymake-solidity solidity-mode powerline csv-mode nyan-mode monokai-theme jedi skewer-mode yasnippet whitespace-cleanup-mode visual-regexp undo-tree string-edit smooth-scrolling smex smartparens simple-httpd restclient prodigy paredit move-text markdown-mode magit ido-vertical-mode ido-completing-read ido-at-point htmlize highlight-escape-sequences guide-key flycheck-pos-tip flx-ido fill-column-indicator elisp-slime-nav dockerfile-mode css-eldoc))
+   (quote
+    (rust-mode projectile git-timemachine clang-format flycheck-clang-analyzer multiple-cursors org-bullets counsel ivy-yasnippet swiper elpy helm ggtags company-c-headers conda flymake-solidity solidity-mode powerline csv-mode nyan-mode monokai-theme jedi skewer-mode yasnippet whitespace-cleanup-mode visual-regexp undo-tree string-edit smooth-scrolling smex smartparens simple-httpd restclient prodigy paredit move-text markdown-mode magit ido-vertical-mode ido-completing-read ido-at-point htmlize highlight-escape-sequences guide-key flycheck-pos-tip flx-ido fill-column-indicator elisp-slime-nav dockerfile-mode css-eldoc)))
  '(realgud:gdb-command-name "gdb-multiarch")
  '(whitespace-style
-   '(face trailing tabs empty indentation space-after-tab space-before-tab tab-mark)))
-
-(require 'flymake-google-cpplint)
-(add-hook 'c++-mode-hook 'flymake-google-cpplint-load)
-
+   (quote
+    (face trailing tabs empty indentation space-after-tab space-before-tab tab-mark))))
 
 ;; put backup files in one spot
 (setq backup-directory-alist
